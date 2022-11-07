3d4fb23261cd4b46cdfd9658b0f789e477d6e8f8
KoLobOK <webkolobok@mail.ru>
2022-11-07 18:07:05 +0300

    features: add menu to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,36 @@
-import headerLogo from "../images/logo-place.svg";
+import {Link, useLocation} from 'react-router-dom';
+
+import headerLogo from '../images/logo-place.svg';
 
 function Header() {
+    let location = useLocation();
+
     return (
         <header className="header page__header">
             <img className="header__logo"
                  src={headerLogo}
                  alt="Логотип с картой России"
             />
+            {location.pathname === "/sign-in" ? (
+                <Link className="header__link" to="/sign-up">
+                    Регистрация
+                </Link>
+            ) : location.pathname === "/sign-up" ? (
+                <Link className="header__link" to="sign-in">
+                    Войти
+                </Link>
+            ) : location.pathname === "/" ? (
+                <div className="header__menu">
+                    <p className="header__email">test@testmail.ru</p>
+                    <button className="header__out-btn"
+                            onClick=''>
+                        Выйти
+                    </button>
+                </div>
+            ) : (
+                ""
+            )
+            }
         </header>
     );
 }
